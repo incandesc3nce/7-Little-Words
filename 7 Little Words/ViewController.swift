6e27bf18b45e7990d1cc73c4f2bb1e9e89499ab3
incandesc3nce <pineaapplepower@gmail.com>
2024-03-15 17:39:12 +0700

    Initial Commit

---
+++ 7 Little Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7 Little Words
+//
+//  Created by Dmitry on 15.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
